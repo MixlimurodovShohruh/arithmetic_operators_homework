0dc4a8f3943a33a9dd192a8904e8009a8dd6a5cb
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2022-12-22 11:28:29 +0500

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,8 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 23
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+x = 23
+x1 = x%10
+x2 = x//10
 #Print the value of the 'answer'
+print(x1+x2)
