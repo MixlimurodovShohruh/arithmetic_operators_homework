a05261da6b3bf06aa98e0147cc85313ff4e2648f
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2022-12-22 10:40:37 +0500

    02

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -3,3 +3,8 @@
 #Create a variable 'answer' and assign it the sum of the number's digits.
 
 #Print the vallue of the 'answer'.
+number = 43 
+answer = 43
+number1 = number%10
+number2 = number//10
+print(number1 + number2)
